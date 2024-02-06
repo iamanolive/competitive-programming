27b7187b4dac83145f2482435cf5e1b9d0257045
iamanolive <theolivequeen.xyz@gmail.com>
2024-02-06 21:48:09 +0300

    4th Feb '24 CP

--- CodeChef/DSA-learning-series/1-complexity-analysis/5-carvans.cpp
+++ CodeChef/DSA-learning-series/1-complexity-analysis/5-carvans.cpp
@@ -1,21 +1,22 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-#define endl '\n'
 
 int main() {
     int t; cin >> t;
     for(int i = 0; i < t; i++) {
         int n; cin >> n;
-        int *maximumSpeed = new int[n];
+        int *maxSpeed = new int[n];
         int *actualSpeed = new int[n];
         for(int j = 0; j < n; j++) {
-            cin >> maximumSpeed[j];
-            actualSpeed[j] = maximumSpeed[j];
-        } int counter = 1;
-        for(int j = 1; j < n; j++) {
-            if(maximumSpeed[j] > maximumSpeed[j-1])
-                actualSpeed[j] = maximumSpeed[j-1];
-            else counter++;
-        } cout << counter << endl;
+            cin >> maxSpeed[j];
+            actualSpeed[j] = maxSpeed[j];
+        } for(int j = 1; j < n; j++)
+            if(actualSpeed[j - 1] < actualSpeed[j])
+                actualSpeed[j] = actualSpeed[j - 1];
+        int counter = 0;
+        for(int j = 0; j < n; j++)
+            if(actualSpeed[j] == maxSpeed[j])
+                counter++;
+        cout << counter << endl;
     }
 }
