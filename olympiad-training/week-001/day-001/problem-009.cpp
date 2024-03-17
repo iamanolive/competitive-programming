ca98fe044b7dbbe2c4d2a4696746b107478175f0
iamanolive <theolivequeen.xyz@gmail.com>
2024-03-17 10:56:45 +0300

    16th March '24 Olympiad Training

--- olympiad-training/week-001/day-001/problem-009.cpp
+++ olympiad-training/week-001/day-001/problem-009.cpp
@@ -1,1 +1,28 @@
-// not solved
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    for(int i = 0; i < t; i++) {
+        long long k, d0, d1;
+        cin >> k >> d0 >> d1;
+        int dCurr = (d0 + d1) % 10;
+        long long sum = d0 + d1;
+        long long remDigits = k - 2;
+        while(remDigits > 0) {
+            if(dCurr == 0) break;
+            if(dCurr % 10 == 2) {
+                if(remDigits % 4 == 0) sum += (remDigits / 4) * 20;
+                else if(remDigits % 4 == 1) sum += (remDigits / 4) * 20 + 2;
+                else if(remDigits % 4 == 2) sum += (remDigits / 4) * 20 + 6;
+                else if(remDigits % 4 == 3) sum += (remDigits / 4) * 20 + 14;
+                break;
+            } else {
+                remDigits--;
+                sum += dCurr;
+                dCurr = (dCurr * 2) % 10;
+            }
+        } if(sum % 3 == 0) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+}
--- olympiad-training/week-001/day-001/problem-011.cpp
+++ olympiad-training/week-001/day-001/problem-011.cpp
@@ -1,27 +1,29 @@
-// not solved
-
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+#define endl '\n'
 
 int main() {
     int t; cin >> t;
     for(int i = 0; i < t; i++) {
         int n; cin >> n;
-        string s; cin >> s; // length = 2 * n
-        int goalsA = 0, goalsB = 0;
-        int missesA = 0, missesB = 0;
+        string s; cin >> s;
+        int goalsA = 0;
+        int goalsB = 0;
+        int shotsA = 0;
+        int shotsB = 0;
         int result = 2 * n;
-        // if s[i] == 0, shot was a miss
-        // else if s[i] == 1, shot was a goal
         for(int j = 0; j < 2 * n; j++) {
-            if(j % 2 == 0 && s[j] == '1') goalsA++;
-            else if(j % 2 == 0 && s[j] == '0') missesA++;
-            else if(j % 2 != 0 && s[j] == '1') goalsB++;
-            else if(j % 2 != 0 && s[j] == '0') missesB++;
-            if(goalsA >= n - 1 && missesB >= 2) {
+            if(j % 2 == 0) {
+                shotsA++;
+                if(s[j] == '1') goalsA++;
+            } else if(j % 2 != 0) {
+                shotsB++;
+                if(s[j] == '1') goalsB++;
+            }
+            if(goalsA > (goalsB + (n - shotsB))) {
                 result = j + 1;
                 break;
-            } else if(goalsB >= n - 1 && missesA >= 2) {
+            } else if(goalsB > (goalsA + (n - shotsA))) {
                 result = j + 1;
                 break;
             }
